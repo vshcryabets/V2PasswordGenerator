f2fb3c26e08e806cc0e4808d41df6e5e0de93de9
V.Shcryabets <vshcryabets@gmail.com>
2012-08-01 10:06:39 +0300

    - read data from console

--- consoletool/main.c
+++ consoletool/main.c
@@ -1,6 +1,18 @@
 #include "v2passgen.h"
+#include "stdio.h"
 
 int main(void) {
-    generatePassword("123456", "test.com", 1);
+    char *masterPassword;
+    char *resource;
+    printf("Enter masterpassword: ");
+    int res = scanf("%as", &masterPassword);
+    masterPassword[res] = 0;
+    printf("Enter resource name: ");
+    res = scanf("%as", &resource);
+    resource[res] = 0;
+    printf("Mp=%s\n", masterPassword);
+    generatePassword(masterPassword, resource, 1);
+    //free(masterPassword);
+    //free(resource);
     return 0;
 }
