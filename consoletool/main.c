767e37e263878df8cc6c6c96320943a078791917
Vladimir Shcriabets <vshcryabets@gmail.com>
2012-08-03 13:49:26 +0300

    - fixed requirments

--- consoletool/main.c
+++ consoletool/main.c
@@ -1,18 +1,31 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
 #include "v2passgen.h"
-#include "stdio.h"
 
 int main(void) {
-    char *masterPassword;
-    char *resource;
-    printf("Enter masterpassword: ");
-    int res = scanf("%as", &masterPassword);
-    masterPassword[res] = 0;
-    printf("Enter resource name: ");
-    res = scanf("%as", &resource);
-    resource[res] = 0;
-    printf("Mp=%s\n", masterPassword);
+	char *masterPassword = NULL;
+	char *resource = NULL;
+	
+	printf("Enter master password: ");
+	size_t bytes_read, nbytes = 0;
+	bytes_read = getline(&masterPassword, &nbytes, stdin);
+	if ( bytes_read < 1 ) {
+		return 1;
+	}
+	masterPassword[bytes_read-1] = 0;
+    
+	printf("Enter resource name: ");
+	bytes_read = getline(&resource, &nbytes, stdin);
+	if ( bytes_read < 1 ) {
+		return 1;
+	}
+	resource[bytes_read-1] = 0;
+
     generatePassword(masterPassword, resource, 1);
-    //free(masterPassword);
-    //free(resource);
-    return 0;
+	free(masterPassword);
+	free(resource);
+
+	return 0;
 }
+
